ada43f64c1c02c8beef5986e999a8fb2a85bbed0
tiga1231 <2913895+tiga1231@users.noreply.github.com>
2020-12-16 14:03:54 -0700

    better landing page

--- demo/mingwei_version.73811e7e.js
+++ demo/mingwei_version.73811e7e.js
@@ -102504,7 +102504,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "localhost" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49553" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60255" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- demo/reyan_lastfm.f062a8a0.js
+++ demo/reyan_lastfm.f062a8a0.js
@@ -102488,7 +102488,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "localhost" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49553" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60255" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
